eada699f80a4c98b85935a7fe594a6291f32e906
Gautamup <upadhyaygautam97@gmail.com>
2025-03-17 15:28:32 +0530

    Added split line and tokenize functionality

--- shellc.c
+++ shellc.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #define BUF_SIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 
 char* shl_reaf_line(void){
     char* line = NULL;
@@ -17,7 +19,36 @@ char* shl_reaf_line(void){
   return line;
 }
 
-
+char **shl_split_line(char *line){
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+  
+    if (!tokens) {
+      fprintf(stderr, "lsh: allocation error\n");
+      exit(EXIT_FAILURE);
+    }
+    
+    token = strtok(line, LSH_TOK_DELIM);
+    
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+    
+        if (position >= bufsize) {
+          bufsize += LSH_TOK_BUFSIZE;
+          tokens = realloc(tokens, bufsize * sizeof(char*));
+          if (!tokens) {
+            fprintf(stderr, "lsh: allocation error\n");
+            exit(EXIT_FAILURE);
+          }
+        }
+    
+        token = strtok(NULL, LSH_TOK_DELIM);
+      }
+      tokens[position] = NULL;
+      return tokens;
+}
 void shl_loop(void){
     char* line;
     char** args;
