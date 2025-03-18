03af03e9866c44c4de73cee42fab35aa5fc00829
Gautamup <upadhyaygautam97@gmail.com>
2025-03-18 22:14:55 +0530

    Added builtins of the shell

--- shellc.c
+++ shellc.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+
 #define BUF_SIZE 1024
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
@@ -49,16 +55,105 @@ char **shl_split_line(char *line){
       tokens[position] = NULL;
       return tokens;
 }
+
+char shl_launch(char **args){
+  pid_t,wpid;
+  int status;
+  pid = fork();
+  //child process pid == 0
+  if(pid == 0){
+    if(execvp(args[0],args) == -1){
+      perror('shl');
+      exit(EXIT_FAILURE);
+    }
+  } else if(pid <0){
+    perror('shl');
+  } else{
+    do{
+      wpid = waitpid(pid,&status,WUNTRACED);
+      }while(!WIFEXITED(status) && !WIFSIGNALED(status))
+  }
+}
+
+int shl_cd(char **args);
+int shl_help(char **args);
+int shl_exit(char **args);
+char *bultin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &shl_cd,
+  &shl_help,
+  &shl_exit
+};
+
+int shl_num_builtins(){
+  return sizeof(bultin_str)/sizeof(char *);
+}
+
+int shl_cd(char **args){
+  if(args[1] == NULL){
+    fprintf(stderr,"shl: expected argument to \"cd\"\n");
+  }
+  else{
+    if(chdir(args[1]) != 0){
+      perror("shl");
+    }
+  }
+  return 1;
+}
+
+int shl_help(char **args){
+  printf("!!!! Welcome to SHL !!!!\n");
+  printf("Type program name and agruements, then hit enter.\n");
+  printf("Type of builtin are:\n");
+
+  for(int i=0;i<shl_num_builtins();i++){
+    printf(" %s\n", bultin_str[i]);
+  }
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int shl_exit(char **args){
+  return 0;
+}
+
+int shl_execute(char **args){
+  if(args[0] == NULL){
+    //Entered empty command
+    return 1;
+  }
+  for(int i=0;i<shl_num_builtins();i++){
+    if(strcmp(args[0],bultin_str[i]) == 0){
+      return (*builtin_func[i])(args);
+    }
+  }
+  return shl_launch(args);
+}
+
 void shl_loop(void){
     char* line;
     char** args;
     int status;
+    bool check = true;
     do{
         printf(">");
         line  = shl_read_line();
         args = shl_split_line(line);
-        status = shl_execute(args);
-
+        int i = 0;
+        while (args[i] != NULL) {
+            if(args[i].isspace()){
+              check = false;
+              break;
+            }
+        }
+        if(check){
+          status = shl_execute(args);
+        }
         free(line);
         free(args);
 
@@ -72,5 +167,5 @@ int main(){
     //Run command loop
     shl_loop();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
